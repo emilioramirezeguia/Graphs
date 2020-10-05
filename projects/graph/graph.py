5889f73c01bdd551bee5d37a8bc177134e1b9c32
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-10-05 12:29:45 -0700

    Add add_edge() graph method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -21,7 +21,7 @@ class Graph:
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
