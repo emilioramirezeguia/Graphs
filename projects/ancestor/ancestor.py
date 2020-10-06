a5029acea40eea5744c86a14f1f1dab984e51d05
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-10-06 13:34:17 -0700

    Create graph out of the dataset

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,24 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # create an empty dictionary as our graph
+    graph = dict()
+    # loop through ancestors
+    for parent_child in ancestors:
+        # for each person in a parent-child relationship
+        for person in parent_child:
+            # if the vertex doesn't exist for that person
+            if person not in graph:
+                # create a vertex in our graph and set it equal to an empty set
+                graph[person] = set()
+        # then add the second person as an adjacent vertex to the first person
+        parent = parent_child[0]
+        child = parent_child[1]
+        graph[parent].add(child)
+
+    return graph
+
+
+# my own testing
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print("Graph", earliest_ancestor(test_ancestors, 3))
