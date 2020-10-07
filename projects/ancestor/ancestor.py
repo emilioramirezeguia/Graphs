923e32ded8174ee0b70ebb2372fcc8e4d6a4db25
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-10-07 14:07:06 -0700

    Finish ancestor exercise

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,7 +1,7 @@
-
 def earliest_ancestor(ancestors, starting_node):
+    ### creating the graph ###
     # create an empty dictionary as our graph
-    graph = dict()
+    graph = {}
     # loop through ancestors
     for parent_child in ancestors:
         # for each person in a parent-child relationship
@@ -13,12 +13,53 @@ def earliest_ancestor(ancestors, starting_node):
         # then add the second person as an adjacent vertex to the first person
         parent = parent_child[0]
         child = parent_child[1]
-        graph[parent].add(child)
+        graph[child].add(parent)
 
-    return graph
+    # searching through our graph
+    # create an empty stack and an empty visited set
+    stack = []
+
+    # initialize the stack with the person we're going to search the earliest ancestor for
+    stack.append([starting_node])
+
+    # edge case: starting node has no parents
+    earliest_ancestor = -1
+    # keep track of the length
+    max_path_length = 1
+
+    # repeat this code as long as there are nodes in the stack
+    while len(stack) > 0:
+        path = stack.pop()
+        last_node = path[-1]
+
+        if len(path) > max_path_length:
+            max_path_length = len(path)
+            earliest_ancestor = last_node
+        elif len(path) >= max_path_length and last_node < earliest_ancestor:
+            max_path_length = len(path)
+            earliest_ancestor = last_node
+
+        for neighbor in graph[last_node]:
+            new_path = path + [neighbor]
+            stack.append(new_path)
+
+    return earliest_ancestor
 
 
 # my own testing
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
                   (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print("Graph", earliest_ancestor(test_ancestors, 3))
+print(earliest_ancestor(test_ancestors, 6))
+
+
+# # # extra code
+# if len(stack) == 2:
+#     if stack[0][-1] < stack[1][-1]:
+#         earliest_ancestor = stack[0][-1]
+#         return earliest_ancestor
+#     else:
+#         earliest_ancestor = stack[1][-1]
+#         return earliest_ancestor
+
+# if len(graph[earliest_ancestor]) == 0:
+#     return -1
